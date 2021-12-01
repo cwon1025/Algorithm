fce1f293f997a13998700b62a36ba4dc5422dd04
skyticket <cheonpyo1025@gmail.com>
2021-12-01 17:30:31 +0900

    commit

--- Sort/Quick/quick.c
+++ Sort/Quick/quick.c
@@ -4,23 +4,23 @@ int number = 10;
 int data[10] = {1, 10, 5, 8, 7, 6, 4, 3, 2, 9};
 
 void quickSort(int *data, int start, int end) {
-	if (start >= end) {
+	if (start >= end) {		// 원소가 1개인 경우
 		return;
 	}
 
-	int key = start;
+	int key = start;		// 키는 첫번째 원소
 	int i = start + 1;
 	int j = end;
 	int temp;
 
-	while (i <= j) {
-		while (data[i] <= data[key]) {
+	while (i <= j) {		// 엇갈릴 때까지 반복
+		while (data[i] <= data[key]) {				// 키 값보다 큰 값을 만날 때까지
 			i++;
 		}
-		while (data[j] >= data[key] && j > start) {
+		while (data[j] >= data[key] && j > start) {	// 키 값보다 작은 값을 만날 때까지
 			j--;
 		}
-		if (i > j) {
+		if (i > j) {			// 현재 엇갈린 상태면 키 값과 교체
 			temp = data[j];
 			data[j] = data[key];
 			data[key] = temp;
